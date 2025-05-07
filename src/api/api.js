6ce581b4fa2966288fffbdcf94048991301e743a
Wesley Cook <wesck37@gmail.com>
2025-05-07 13:38:08 -0700

    Fix backend port connection

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,30 @@
+// import axios from 'axios';
+
+// // Function to send data to the backend
+// export const sendDataToBackend = async (data) => {
+//   try {
+//     const response = await axios.post('http://localhost:5000', data, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error sending data to the backend:', error);
+//     throw error;
+//   }
+// };
+
 import axios from 'axios';
+
+// Load the backend base URL from environment variables
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 // Function to send data to the backend
 export const sendDataToBackend = async (data) => {
   try {
-    const response = await axios.post('http://localhost:5000', data, {
+    const response = await axios.post(`${API_URL}/api/data`, data, {
       headers: {
         'Content-Type': 'application/json',
       },
